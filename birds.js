5d9ffca1d42dc00e6d6c279efe5da64639615423
metaversepeace <jamesrbroucek@gmail.com>
2021-10-18 17:41:06 -0700

    first commit

---
+++ birds.js
@@ -0,0 +1,52 @@
+
+let choice = document.querySelector("#birds")
+
+let canary = choice[1]
+
+/*menu.addEventListener('click', function() {
+    
+    console.log('You chose canary!')
+    
+    }
+
+/*let canary = menu[1]
+
+var canarySentence = 'You chose canary!'
+
+
+
+  /* {name: 'Canary', img: 'canary.jpg', sound: 'canary.mp3'},
+
+   {name: 'Grey Parrot', img: 'Grey Parrot.jpg' , sound: 'parrot_sounds_short.wav'},
+
+   {name: 'Rooster', img: 'rooster.jpg', sound: 'rooster.mp3'},
+
+   {name: 'Western Meadowlark', img: 'western-meadowlark.jpg', sound: 'meadowlark.wav'}*/
+]
+//Canary = birds[1]; Grey Parrot = birds[2]; Rooster = birds[3]; Western Meadowlark[4]//
+
+
+/*var selection = document.#birds.value;
+
+
+
+//document.getElementById('bird').addEventListener('onchange', function() {
+  //  gbOrder = gbOrder + 1; console.log(gbOrder);
+   // gbTotalCell.textContent = gbOrder;
+    // cookieTotalCell.textContent = gbOrder + ccOrder + sugarOrder})
+
+    //var x = document.getElementById('birds').value;     document.getElementById("choice").innerHTML = "You chose: "
+
+    /*let menu = [
+
+   {name: 'Canary', img: 'canary.jpg', sound: 'canary.mp3'},
+
+   {name: 'Grey Parrot', img: 'Grey Parrot.jpg' , sound: 'parrot_sounds_short.wav'},
+
+   {name: 'Rooster', img: 'rooster.jpg', sound: 'rooster.mp3'},
+
+   {name: 'Western Meadowlark', img: 'western-meadowlark.jpg', sound: 'meadowlark.wav'}*/
+]
+//Canary = birds[1]; Grey Parrot = birds[2]; Rooster = birds[3]; Western Meadowlark[4]//
+
+//var canary = document.getElementById('birds').value('Canary') 
